7832ffd6b526de392db12e689dfe132d143101a5
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-04-15 14:25:25 +0700

    contact list

--- src/Lession1_arraylist/ArrayPerson.java
+++ src/Lession1_arraylist/ArrayPerson.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ArrayPerson {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Shachin", 47));
+        people.add(new Person("S", 47));
         people.add(new Person("Chris", 34));
         people.add(new Person("Rajeev", 25));
         people.add(new Person("David", 31));
@@ -18,11 +18,23 @@ public class ArrayPerson {
             return person1.getAge() - person2.getAge();
         });
 
-        people.sort(Comparator.comparingInt(Person::getAge));
+        Person pp = new Person("david", 47);
+        for(Person x: people){
+            if(pp.getName() == x.getName()){
+                System.out.println("hello" + x.getName());
+            }
+        }
 
-        System.out.println("Sorted Person List by Age: " + people);
+//        people.forEach(peoples ->{
+//            System.out.println(peoples+ "index" + people.indexOf(peoples));
+//        });
 
-        Collections.sort(people, Comparator.comparing(Person::getName));
-        System.out.println("Sorted Person List by Name: " + people);
+
+//        people.sort(Comparator.comparingInt(Person::getAge));
+//
+//        System.out.println("Sorted Person List by Age: " + people);
+//
+//        Collections.sort(people, Comparator.comparing(Person::getName));
+//        System.out.println("Sorted Person List by Name: " + people);
     }
 }
